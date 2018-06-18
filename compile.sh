57207dd1b632040e1bad29812aab3f1c8b44d970
Quinn Reilly <Quinn.Reilly@hotmail.com>
2018-06-18 20:34:19 +0100

    progress 1

---
+++ compile.sh
@@ -0,0 +1,13 @@
+clear
+
+clear
+
+clear
+
+g++ main.cpp inc/common.h -lsfml-graphics -lsfml-window -lsfml-system
+
+cd obj
+
+./run
+
+cd ..
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "inc/common.h"
+
+int main()
+{
+	
+	
+	return 0;
+}
